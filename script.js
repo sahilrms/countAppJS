7cab41e29014031a5b8fa4f1c9474526e52f2ccc
sahilHussain <sahil.hussain@travel2kashmir.com>
2023-03-15 10:00:50 +0530

    final commit

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let dec_btn = document.getElementById('dec');
 let inc_btn = document.getElementById('inc');
+let reset_btn = document.getElementById('reset');
 let count = document.getElementById('count');
 
 dec_btn.addEventListener("click", decreaseCount); 
 inc_btn.addEventListener("click", increaseCount);
+reset_btn.addEventListener("click",resetCount);
 
 function decreaseCount(){
     let value=Number(count.innerHTML);
@@ -12,4 +14,9 @@ function decreaseCount(){
 function increaseCount(){
     let value=Number(count.innerHTML);
     count.innerHTML=value+1;
+}
+
+function resetCount(){
+    let value=Number(count.innerHTML);
+    count.innerHTML=1;
 }
